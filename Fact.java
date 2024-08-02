201fc8944506a5a8943e8f59bc6e2d02e5f98f8d
armanshaikh9850 <156654330+armanshaikh9850@users.noreply.github.com>
2024-08-02 22:43:12 +0530

    Fact.java

--- Fact.java
+++ Fact.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.util.Scanner; // Scanner Class for reading input from user
 public class Fact {
-    public static void main(String args[])
+    public static void main(String args[]) // Main function 
     {
-        try(Scanner sc = new Scanner(System.in))
+        try(Scanner sc = new Scanner(System.in)) // Scanner Syntax
         {
             System.out.print("Enter a Number : ");
-            int n = sc.nextInt();
+            int n = sc.nextInt(); // Converting the data type from default string to integer
             int fact=1;
             for(int i=2;i<=n;i++)
             {
-                fact*=i;
+                fact*=i; // Formula placed inside a basic for loop
             }
             System.out.println("Factorial of "+n+" is : "+fact);
             
