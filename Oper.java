e455506412b0584d46d75907df458178a9fccc51
Arman Shaikh <156654330+armanshaikh9850@users.noreply.github.com>
2024-08-03 19:37:03 +0530

    Add files via upload

---
+++ Oper.java
@@ -0,0 +1,27 @@
+public class Oper
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=1;
+        int c=a|=b; // Using  Bitwise Operator |
+        System.out.println("Answer  | : " +c);
+        System.out.println("Answer % : "+a%b); // Using  Bitwise Operator %
+        c=a&b;
+        System.out.println("Answer & : "+c); // Using  Bitwise Operator &
+        c=a^b;
+        System.out.println("Answer ^ : "+c); // Using  Bitwise Operator ^
+        c=a<<b;
+        System.out.println("Answer << : "+c); // Using  Bitwise Operator <<
+        c=a>>b;
+        System.out.println("Answer >> : "+c); // Using  Bitwise Operator >>
+        c=a>>>b;
+        System.out.println("Answer >>> : "+c); // Using  Bitwise Operator >>>
+        System.out.println("Answer ~ : "+ ~a);
+        System.out.println("Answer && : "+(a > 6 && b < 10)); // Using  Bitwise Operator &&
+        System.out.println("Answer || : "+(a > 6 || b < 10)); // Using  Bitwise Operator ||
+        System.out.println("Answer ! : "+!(a > 5 || b < 1)); // Using  Bitwise Operator !
+        System.out.println("Answer ++ : "+ ++a); // Using Increment Operator
+        System.out.println("Answer ++ : "+ --b); // Using Decrement Operator
+    }
+}
