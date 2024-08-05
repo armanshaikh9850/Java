6bde08c372a1db415965b4a5fb0a80c466d5d0cc
Arman Shaikh <156654330+armanshaikh9850@users.noreply.github.com>
2024-08-05 21:15:33 +0530

    Java testing
    Fibonacci Series

---
+++ Fib.java
@@ -0,0 +1,21 @@
+public class Fib {
+    public static void main(String args[]) {
+        // Range of the Fibonacci Series
+        int n = 7;
+        int prev = 0, curr = 1, c;
+
+        // Print the Initial Two Numbers
+        System.out.print("Fibonacci series upto " + n + " : " + prev  + "  " + curr);
+
+        for (int i = 2; i < n; i++) {
+            // Calculate Next Number as Sum of Previous Two
+            c = prev + curr;
+
+            // Update Previous Two Numbers for Next Iteration
+            prev = curr;
+            curr = c;
+
+            System.out.print("  " + c); // Print Rest of the Numbers.
+        }
+    }
+}
